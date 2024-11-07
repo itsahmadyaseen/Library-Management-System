9fa165979a4f22e6770bcb00a2ef2f4dab2f64b4
itsahmadyaseen <itsahmadyaseen@gmail.com>
2024-11-07 11:19:41 +0530

    handled logic for undefined routes and added protected routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import {
   Route,
   Routes,
   useLocation,
+  Navigate,
 } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import ManageBooks from "./Pages/admin/ManageBooks";
@@ -18,13 +19,13 @@ import AddBook from "./Pages/Books/AddBook";
 import Borrow from "./Pages/admin/Borrow";
 import GetMembers from "./Pages/admin/GetMembers";
 import Transaction from "./Pages/member/Transaction";
+import ProtectedRoute from "./ProtectedRoute";
 
 const AppRoutes = () => {
   const location = useLocation();
   const isAuthPage =
     location.pathname === "/login" || location.pathname === "/signup";
   const role = localStorage.getItem("role");
-  
 
   return (
     <>
@@ -32,29 +33,95 @@ const AppRoutes = () => {
         {!isAuthPage && role === "admin" && <Sidebar />}
         {!isAuthPage && role === "member" && <Navbar role={role} />}
         <div
-          className={`flex-1 ${!isAuthPage && role === "admin" ? "ml-0" : ""}  transition-all duration-300 ease-in-out`}
+          className={`flex-1 ${
+            !isAuthPage && role === "admin" ? "ml-0" : ""
+          }  transition-all duration-300 ease-in-out`}
         >
           {/* Add margin-left if sidebar is present */}
 
           <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
-            <Route path="/" element={<ViewBooks />} />
-            <Route path="/profile" element={<Profile />} />
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <ViewBooks />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/profile"
+              element={
+                <ProtectedRoute>
+                  <Profile />
+                </ProtectedRoute>
+              }
+            />
             {role === "admin" && (
               <>
-                <Route path="/admin/manage-books" element={<ManageBooks />} />
-                <Route path="/admin/borrow" element={<Borrow />} />
+                <Route
+                  path="/admin/manage-books"
+                  element={
+                    <ProtectedRoute>
+                      <ManageBooks />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/admin/borrow"
+                  element={
+                    <ProtectedRoute>
+                      <Borrow />
+                    </ProtectedRoute>
+                  }
+                />
 
-                <Route path="/admin/add-book" element={<AddBook />} />
-                <Route path="/admin/get-member" element={<GetMembers />} />
+                <Route
+                  path="/admin/add-book"
+                  element={
+                    <ProtectedRoute>
+                      <AddBook />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/admin/get-member"
+                  element={
+                    <ProtectedRoute>
+                      <GetMembers />
+                    </ProtectedRoute>
+                  }
+                />
 
-                <Route path="/admin/return" element={<Return />} />
+                <Route
+                  path="/admin/return"
+                  element={
+                    <ProtectedRoute>
+                      <Return />
+                    </ProtectedRoute>
+                  }
+                />
               </>
             )}
 
-            <Route path="/view-books" element={<ViewBooks />} />
-            <Route path="/member/transactions" element={<Transaction />} />
+            <Route
+              path="/view-books"
+              element={
+                <ProtectedRoute>
+                  <ViewBooks />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/member/transactions"
+              element={
+                <ProtectedRoute>
+                  <Transaction />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="*" element={<Navigate to="/" />} />
           </Routes>
         </div>
       </div>
