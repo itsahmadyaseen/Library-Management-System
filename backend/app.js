616526fe33fe4686beb2db4f301a76ac6e7ee9d9
itsahmadyaseen <itsahmadyaseen@gmail.com>
2024-11-07 12:53:59 +0530

    updated cors

--- backend/app.js
+++ backend/app.js
@@ -15,8 +15,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173", 
-    credentials: true, 
+    origin: "http://localhost:5173, https://readers-space-eight.vercel.app",
+    credentials: true,
   })
 );
 
