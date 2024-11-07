fc627bad638495bf9af854d4771b053a7995a8c4
itsahmadyaseen <itsahmadyaseen@gmail.com>
2024-11-07 14:34:16 +0530

    cors

--- backend/app.js
+++ backend/app.js
@@ -14,21 +14,9 @@ connection();
 app.use(express.json());
 app.use(cookieParser());
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://readers-space-eight.vercel.app",
-];
-
 app.use(
   cors({
-    origin: function (origin, callback) {
-      // Allow requests with no origin (like mobile apps or curl requests)
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: '*',
     credentials: true,
   })
 );
