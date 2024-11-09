5ba48c0d22952d8feb123659373dd5c543cd0a82
itsahmadyaseen <itsahmadyaseen@gmail.com>
2024-11-09 11:16:00 +0530

    corrected api

--- frontend/axiosInstance.jsx
+++ frontend/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://read-space-nine.vercel.app/ap1/v1",
+  baseURL: "https://read-space-nine.vercel.app/api/v1",
   withCredentials: true, // Send cookies along with requests
 });
 
